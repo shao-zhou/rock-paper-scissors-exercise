d10eab417c8e60deda61bc85cdd3fe1c71d3e139
shao-zhou <sz745@stern.nyu.edu>
2019-06-05 19:46:24 -0400

    Determine winning option

--- game.py
+++ game.py
@@ -28,5 +28,24 @@ print("COMPUTER CHOICE:", computer_choice)
 
 # DETERMINE WINNER
 
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# same selection is a tie
+
+if user_choice == computer_choice:
+    print("TIE")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("ROCK")
+elif user_choice == "paper" and computer_choice == "rock":
+    print("ROCK")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("ROCK")
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
 
 # DISPLAY FINAL OUTPUTS / OUTCOMES
